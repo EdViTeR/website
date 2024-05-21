151091ac5c68b98abfc836b137f02749e8817153
EdViTeR <maxpivnenko@mail.ru>
2024-05-21 15:12:21 +0300

    сессии при авторизации и регистрации

--- cabinet/save_user.php
+++ cabinet/save_user.php
@@ -1,26 +1,36 @@
 <?php
-
+session_start();
 $root = $_SERVER['DOCUMENT_ROOT'];
 require_once $root . '/database/connect.php';
 require_once $root . '/database/database.php';
+
 
 $name 			        = $_POST['name'];
 $email 			        = $_POST['email'];
 $repeate_password       = $_POST['repeat_password'];
 $password               = $_POST['password'];
-if ($repeate_password === $password) {
-    $password              = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $query = ("INSERT INTO `user` SET `name` = :name, `email` = :email, `password` = :password");
-    $params = [
-        'name'          => $name,
-        'email'         => $email,
-        'password'      => $password,
-    ];
-    $stmt = $dbo->prepare($query);
-    $stmt->execute($params);
-    header('Location: sign_in.php');
+$user = user($dbo, $email);
+
+if (isset($user) && !empty($user)) {
+    $_SESSION['message'] = 'Данный Email уже зарегистрирован!';
+    header("Location: sign_up.php");
 } else {
-    header('Location: sign_up.php');
+    if ($repeate_password === $password) {
+        $password              = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $query = ("INSERT INTO `user` SET `name` = :name, `email` = :email, `password` = :password");
+        $params = [
+            'name'          => $name,
+            'email'         => $email,
+            'password'      => $password,
+        ];
+        $stmt = $dbo->prepare($query);
+        $stmt->execute($params);
+        $_SESSION['access'] = 'Вы успешно зарегистрированы!';
+        header('Location: sign_in.php');
+    } else {
+        $_SESSION['message'] = 'Пароли не совпадают!';
+        header('Location: sign_up.php');
+    }
 }
 
 
