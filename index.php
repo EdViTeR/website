4bba829bc7b6e7a85c2b1a0b7fe2879b281fe77d
karasique22 <timofeidudin@mail.ru>
2024-05-21 20:55:33 +0300

    header.php

--- index.php
+++ index.php
@@ -13,32 +13,11 @@ require_once 'footer.php'
 </head>
 
 <body>
-	<header class="header">
-		<div class="logo">
-			<a href="/"><img src="assets/img/logo.png"></a>
-		</div>
-		<div class="nav-toggle">
-			<img src="assets/img/icon.png" alt="Меню" onclick="toggleMenu()">
-		</div>
-		<nav class="nav">
-			<ul>
-				<li><a href="school.php">ШКОЛА</a></li>
-				<li><a href="#">СТАТЬИ</a></li>
-				<li><a href="#">КОЛЛЕКЦИИ</a></li>
-				<li><a href="#">О ПРОЕКТЕ</a></li>
-				<li><a href="cabinet/sign_in.php">КАБИНЕТ</a></li>
-			</ul>
-		</nav>
-		<div class="dropdown-menu">
-			<ul>
-				<li><a href="#">ШКОЛА</a></li>
-				<li><a href="#">СТАТЬИ</a></li>
-				<li><a href="#">КОЛЛЕКЦИИ</a></li>
-				<li><a href="#">О ПРОЕКТЕ</a></li>
-				<li><a href="#">КАБИНЕТ</a></li>
-			</ul>
-		</div>
-	</header>
+	<?php
+	require_once 'header.php';
+	$header = new Header();
+	$header->render();
+	?>
 	<div class="content">
 		<!-- Здесь находится ваш текущий контент -->
 		<div class="video-overlay">
