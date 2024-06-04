223e104fe5cb3728f2d42665560d7dda3cc120e8
karasique22 <timofeidudin@mail.ru>
2024-06-04 14:42:51 +0300

    fix footer layout

--- footer.php
+++ footer.php
@@ -5,31 +5,24 @@ class Footer
 	{
 ?>
 		<footer>
-			<div class="flex-container">
-				<div class="col">
-					<div class="contacts">
-						<div class="group">
-							<h2>8 (495) 999-09-09</h2>
-							<p>Контактный центр</p>
-						</div>
-						<div class="group">
-							<h2>8 (495) 999-09-09</h2>
-							<p>Контактный центр</p>
-						</div>
-					</div>
-					<div class="group">
-						<h2>Есть вопрос или предложение?</h2>
-						<p>Напишите (ссылка) — ответственному за ...</p>
-					</div>
+			<div class="footer-grid">
+				<div class="group top-left">
+					<h2 class="phone-number">8 (495) 999-09-09</h2>
+					<p>Контактный центр</p>
 				</div>
-
-				<div class="col">
-					<div class="group">
-						<h2>г. Москва, ул. Малая Калужская, д. 1, каб. 22</h2>
-						<p><a href="mailto:school@mail.ru">school@mail.ru</a></p>
-					</div>
-					<p>Мы используем файлы cookie, для персонализации сервисов и повышения удобства пользования сайтом. Если вы не согласны на их использование, поменяйте настройки браузера.</p>
+				<div class="group top-middle">
+					<h2 class="phone-number">8 (495) 999-09-09</h2>
+					<p>Контактный центр</p>
 				</div>
+				<div class="group top-right">
+					<h2>г. Москва, ул. Малая Калужская, д. 1, каб. 22</h2>
+					<p><a href="mailto:school@mail.ru">school@mail.ru</a></p>
+				</div>
+				<div class="group bottom-left">
+					<h2>Есть вопрос или предложение?</h2>
+					<p>Напишите (ссылка) — ответственному за ...</p>
+				</div>
+				<p class="bottom-right">Мы используем файлы cookie, для персонализации сервисов и повышения удобства пользования сайтом. Если вы не согласны на их использование, поменяйте настройки браузера.</p>
 			</div>
 			<div class="social-group">
 				<a href="https://youtube.com"><img src="/assets/img/youtube.svg" alt="Youtube"></a>
