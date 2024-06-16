e40297ab0591a27e6eb7ce47562e43c0ad6edc95
karasique22 <timofeidudin@mail.ru>
2024-06-17 02:48:54 +0300

    fix search

--- search_author.php
+++ search_author.php
@@ -2,10 +2,11 @@
 session_start();
 include "database/database.php";
 if (isset($_POST) && !empty($_POST)) {
-	$name = mb_strtolower($_POST["author"]);
+	$name = strtolower($_POST["author"]);
 	$data = author_name($dbo, $name);
+
 	$_SESSION['search_ornaments'] = $data;
 	header("Location: collections.php");
+} else {
+	header("Location: collections.php");
 }
-header("Location: collections.php");
-?>
